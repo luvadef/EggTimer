1de1cb09543561409177d5422df1567f859bc198
Luis Valdes <luisvaldes@Luiss-MacBook-Pro.local>
2021-01-27 12:27:02 -0300

    first commit

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    	
+    @IBOutlet weak var eggLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    let eggTimes: [String:Int] = ["Soft": 5, "Medium": 420, "Hard": 720]
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    var player: AVAudioPlayer!
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        eggLabel.text = hardness
+        totalTime = eggTimes[hardness]!
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateTimer(){
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+        }else {
+            timer.invalidate()
+            playSound()
+            eggLabel.text = "Done!"
+        }
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+
+}
