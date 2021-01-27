e3b13a8bba4672596ae0657e4eae866596d83390
Luis Valdes <luisvaldes@Luiss-MacBook-Pro.local>
2021-01-27 13:45:28 -0300

    cambio temporizador

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     	
     @IBOutlet weak var eggLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
-    let eggTimes: [String:Int] = ["Soft": 5, "Medium": 420, "Hard": 720]
+    let eggTimes: [String:Int] = ["Soft": 300, "Medium": 420, "Hard": 720]
     var timer = Timer()
     var totalTime = 0
     var secondsPassed = 0
